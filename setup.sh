1397f5ef741a6ff533c2050999a6a23f97150af1
Aaron <62109592+Aweymouth13@users.noreply.github.com>
2025-02-11 13:23:14 +0000

    backup before houeskeeping might git revert lol

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@
 
 sudo apt update
 sudo apt install -y python3 python3-venv python3-pip
-pip install numpy matplotlib scipy sympy pillow jupyterlab
+pip install numpy matplotlib scipy sympy pillow jupyterlab pandoc
